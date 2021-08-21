d12041a4bd33ed5e90ae168e4171f017f9a57996
Dustin Atwood <dustin@atwood.email>
2021-08-20 19:20:57 -0500

    added skill section

--- src/styles/homeStyle.js
+++ src/styles/homeStyle.js
@@ -9,13 +9,44 @@ const homeStyle = (theme) => ({
 	body: {
 		color: theme.palette.primary.light,
 		fontFamily: [ 'Nunito Sans', 'sans-serif' ],
-		fontSize: '1rem'
+		fontSize: '1rem',
+		textShadow: "1px 1px rgb(64 64 64 / 90%)"
+	},
+	colored: {
+		color: theme.palette.primary.light,
+		textShadow: "1px 1px rgb(64 64 64 / 90%)"
 	},
 	link: {
 		color: theme.palette.secondary.dark,
 		'&:hover': {
 			color: theme.palette.secondary.main
 		}
+	},
+	papers: {
+		background: 'rgba(0,0,0,0.7)',
+	},
+	skills: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+		textShadow: "1px 0 black"
+  },
+	skillset: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexWrap: 'nowrap',
+		flex: 1
+  },
+	skillList: {
+		flex: 1
+	},
+	largeGap: {
+		minHeight: '200px'
+	},
+	smallGap: {
+		minHeight: '100px'
 	}
 });
 
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Typography, Grid, Fade } from '@material-ui/core';
+import { Typography, Grid, Fade, Paper, Divider } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import homeStyle from '../styles/homeStyle';
+import { List, ListItem } from '@material-ui/core'
+import dataSkills from '../data/skills.json'
+
 
 const Home = ({ classes }) => {
+
+	let skillChart = dataSkills.map((data) => 
+		<div key={data.name} className={classes.skillList} >
+			<div className={classes.skills}>
+				<Typography variant="title" className={classes.colored}>
+					{data.name}
+				</Typography>
+			</div>
+			<List dense={true} >
+				{
+					data.list.map((skill) => 
+						<ListItem key={skill} className={classes.skillset}>
+							<Typography variant="caption" className={classes.body}>
+								{skill}
+							</Typography>
+							{/* <ListItemText primary="Single-line item">{skill}</ListItemText> */}
+						</ListItem>
+					)
+				}
+			</List>
+		</div>
+	)
+
 	return (
 		<div>
 			<Grid container spacing={24}>
@@ -13,19 +39,52 @@ const Home = ({ classes }) => {
 						Hello, I'm Dustin
 					</Typography>
 				</Grid>
+
 				<Grid item xs={12}>
 					<Fade in timeout={2000}>
 						<Typography variant="subheading" className={classes.body}>
-							I’m a passionate software engineer with a focus on delivering quality applications that are
-							both highly efficient and security conscious. Check out
-							<Link to="/about" className={classes.link}>
-								{' about me '}
-							</Link>to learn more or feel free to
-							<Link to="/contact" className={classes.link}>
-								{' contact me. '}
-							</Link>
+							I’m a software engineer with a focus on delivering high
+							quality web solutions. Check out my latest 
+							<Link to="/projects" className={classes.link}> Projects</Link>. 
+							Feel free to 
+							<Link to="/contact" className={classes.link}> reach out </Link> or email
+							about new opportunities.
 						</Typography>
 					</Fade>
+				</Grid>
+				<Grid item xs={12} className={classes.largeGap}>
+					<Divider />
+				</Grid>
+				<Grid item xs={12}>
+					<Divider />
+					<div className={classes.skills}>
+						<Fade in timeout={3000}>
+							<Typography variant="subheading" className={classes.body}>
+								Skills
+							</Typography>
+							</Fade>
+					</div>
+				</Grid>
+				<Grid item xs={12} className={classes.smallGap}>
+					<Divider />
+				</Grid>
+				<Grid item xs={12} >
+					<Fade in timeout={4000}>
+						<Paper className={classes.papers}>
+							<div className={classes.skillset}>
+								{ skillChart }
+							</div>
+						</Paper>
+					</Fade>
+					
+					<div className={classes.skills}>
+						<Fade in timeout={10000}>
+							<Typography variant="caption" className={classes.body}>
+								and more...
+							</Typography>
+						</Fade>
+					</div>
+					
 				</Grid>
 			</Grid>
 		</div>
